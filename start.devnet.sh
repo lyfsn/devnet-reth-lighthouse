b760507c0dae793e9f481bebb41fe2fb0d03867f
lyfsn <dev.wangyu@proton.me>
2023-12-07 17:09:35 +0800

    up

--- start.devnet.sh
+++ start.devnet.sh
@@ -1,9 +1,7 @@
 
-mkdir -p config-example
 mkdir -p el-cl-genesis-data
 mkdir -p execution-data
 mkdir -p consensus-data
-mkdir -p validator-keys
 
 
 export IP_ADDRESS=88.99.94.109
--- start.local.sh
+++ start.local.sh
@@ -1,9 +1,7 @@
 
-mkdir -p config-example
 mkdir -p el-cl-genesis-data
 mkdir -p execution-data
 mkdir -p consensus-data
-mkdir -p validator-keys
 
 
 export IP_ADDRESS=127.0.0.1
